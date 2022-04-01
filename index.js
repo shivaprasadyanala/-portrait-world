c650defcf6418f982cb1761d4d561340872ded66
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-04-01 17:05:24 +0530

    added ejs

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express')
 const multer = require('multer')
 var bodyParser = require('body-parser');
 var Image = require('./models/imageModel')
+const bb = require('express-busboy');
 const mongoose = require('mongoose');
 const { find } = require('./models/imageModel');
+const ejs = require("ejs");
+path = require('path')
 var app = express();
+bb.extend(app, {
+    upload: true,
+    path: '/path/to/save/files',
+    allowedPath: /./
+});
 app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//rendering template
+// app.set("views", path.join(__dirname, "./models/views"))
+app.set('view engine', 'ejs');
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads/')
@@ -27,29 +38,43 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1024 * 1024 * 5
+        fileSize: 1024 * 1024 * 50
     },
     fileFilter: fileFilter
 })
 
 
 //get image
-app.get("/images/", async(req, res) => {
-        const image = await Image.find();
-        res.status(200).send(image)
+app.get('/', async(req, res) => {
+    res.render('pages/index')
+})
+app.get("/images", async(req, res) => {
+
+        const images = await Image.find();
+        // console.log(images);
+        res.render('pages/images', {
+                images: images
+            })
+            // res.status(200).send(images)
     })
     //get image by id
 app.get("/image/:id", async(req, res) => {
-        const image = await Image.findById(req.params.id);
-        if (!image)
-            res.status(200).send("image not availble");
-        else
-            res.status(200).send(image)
+    const image = await Image.findById(req.params.id);
+    if (!image)
+        res.status(200).send("image not availble");
+    else
+        res.status(200).send(image)
+})
+
+
+app.get("/upload", (req, res) => {
+        res.render('pages/upload')
     })
     //post image 
 app.post("/upload", upload.single('myImage'), (req, res, next) => {
-        console.log(req.file);
+        console.log(req.body);
         console.log(req.body.title);
+        console.log(req.file);
         const image = new Image({
                 title: req.body.title,
                 content: req.body.content,
