3e7eb2ff573d3cdc45bed17ba026003fcc5dad35
shivaprasadyanala <shivaprasadyanala@gmail.com>
2022-04-04 17:27:38 +0530

    added upload image in edit

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const express = require('express')
 const multer = require('multer')
 var bodyParser = require('body-parser');
 var Image = require('./models/imageModel')
-const bb = require('express-busboy');
+const methodOverride = require('method-override')
+
 const mongoose = require('mongoose');
 const { find } = require('./models/imageModel');
 const ejs = require("ejs");
 path = require('path')
 var app = express();
-bb.extend(app, {
-    upload: true,
-    path: '/path/to/save/files',
-    allowedPath: /./
-});
+app.use(methodOverride('_method'))
 app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -60,6 +57,7 @@ app.get("/images", async(req, res) => {
     //get image by id
 app.get("/image/:id", async(req, res) => {
     const image = await Image.findById(req.params.id);
+    console.log(image);
     if (!image)
         res.status(200).send("image not availble");
     else
@@ -70,37 +68,49 @@ app.get("/image/:id", async(req, res) => {
 app.get("/upload", (req, res) => {
         res.render('pages/upload')
     })
-    //post image 
+    //post image
 app.post("/upload", upload.single('myImage'), (req, res, next) => {
-        console.log(req.body);
-        console.log(req.body.title);
-        console.log(req.file);
-        const image = new Image({
-                title: req.body.title,
-                content: req.body.content,
-                heading: req.body.heading,
-                imageUrl: req.file.path
-            }
-
-        )
-        image.save()
-            .then(result => {
-                res.status(201).json(result);
-            })
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.file);
+    const image = new Image({
+        title: req.body.title,
+        content: req.body.content,
+        heading: req.body.heading,
+        imageUrl: req.file.path
     })
-    //edit image 
-app.put("/edit/:id", upload.single('myImage'), (req, res, next) => {
-        // console.log(req.file);
-        const cimage = Image.findById(req.params.id);
-        // console.log(cimage.obj);
+    image.save()
+        .then(result => {
+            // res.status(201).json(result);
+            res.redirect('/images')
+        })
+})
+
+
+app.get('/edit/:id', async(req, res) => {
+        const image = await Image.findById(req.params.id);
+        if (image)
+            res.render('pages/edit', { image: image })
+    })
+    //edit image
+app.put("/edit/:id", upload.single('myImage'), async(req, res, next) => {
+
+        const cimage = await Image.findById(req.params.id);
+        console.log(cimage);
+        if (req.file != null) {
+            imgurl = req.file.path
+        } else {
+            imgurl = cimage.imageUrl;
+        }
         cimage.updateOne({
                 title: req.body.title,
                 content: req.body.content,
                 heading: req.body.heading,
-                imageUrl: req.file.path
+                imageUrl: imgurl
+
             })
             .then(result => {
-                res.status(201).json(result);
+                res.redirect('/images')
             })
     })
     //delete product
@@ -109,7 +119,8 @@ app.delete("/delete/:id", async(req, res, next) => {
         const cimage = Image.findById(req.params.id);
         if (cimage) {
             await cimage.deleteOne();
-            res.status(201).send("image got deleted")
+            // res.status(201).send("image got deleted")
+            res.redirect('/images')
         }
 
     })
